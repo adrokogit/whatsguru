c81f8c5779959751ff20b45c48bd519932436b0c
adrian.fernandez <adrianfernandezalvarez02@gmail.com>
2025-10-31 11:01:05 +0100

    log change

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import { schedulerRouter } from "./api/schedulerApi";
 import { apiKeyGuard } from "./middleware/apiKey";
+import { logger } from "./utils/logger";
 
 const app = express();
 app.use(morgan("dev"));
@@ -12,5 +13,5 @@ app.use("/api/schedules", apiKeyGuard, schedulerRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🌐 API escuchando en puerto ${PORT}`);
+  logger.info(`🌐 API escuchando en puerto ${PORT}`);
 });
