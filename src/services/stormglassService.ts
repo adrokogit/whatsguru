f91e9f673ce9d3d600b88769d802e73a33620ab8
adrian.fernandez <adrianfernandezalvarez02@gmail.com>
2025-10-31 12:16:10 +0100

    Added cache for stormglass requests

--- src/services/stormglassService.ts
+++ src/services/stormglassService.ts
@@ -13,10 +13,18 @@ const DEFAULT_PARAMS = [
   "visibility",
 ].join(",");
 
+let surfCache: Map<string, { json: any; date: Date }> = new Map();
+const THRESHOLD_TIME = 30 * 60 * 1000;
+
 export async function getStormglassPoint(lat: number, lng: number) {
   const apiKey = process.env.STORMGLASS_API_KEY;
   if (!apiKey) {
     throw new Error("STORMGLASS_API_KEY not set");
+  }
+
+  const key = getCoordsKey(lat, lng);
+  if (checkCache(key)) {
+    return surfCache.get(key).json;
   }
 
   const url = `${STORMGLASS_BASE}?lat=${lat}&lng=${lng}&params=${DEFAULT_PARAMS}&source=noaa`;
@@ -30,5 +38,28 @@ export async function getStormglassPoint(lat: number, lng: number) {
   }
 
   const data = await res.json();
+  surfCache.set(key, { json: data, date: new Date() });
   return data as any;
+}
+
+function checkCache(coords: string) {
+  if (surfCache.has(coords)) {
+    if (lessThanThresholdTime(surfCache.get(coords).date)) {
+      return true;
+    } else {
+      surfCache.delete(coords);
+      return false;
+    }
+  }
+  return false;
+}
+
+function lessThanThresholdTime(fecha: Date): boolean {
+  const now = new Date();
+  const timeMillis = now.getTime() - fecha.getTime();
+  return timeMillis < THRESHOLD_TIME;
+}
+
+function getCoordsKey(lat: number, lng: number) {
+  return `${lat},${lng}`;
 }
